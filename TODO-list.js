8700f18400249819566094e8b27342f4b5848040
zahrakaabi <kaabizahra@gmail.com>
2022-10-17 17:38:36 +0100

    chore: Style scroll-bar

--- TODO-list.js
+++ TODO-list.js
@@ -1,7 +1,7 @@
 // @ TO DO : Implement msg received box --------------------------> done
 // @ TO DO : Implement links -------------------------------------> done
-// @ TO DO : Fix font appearing
-// @ TO DO : Style scroll-bar
+// @ TO DO : Fix font appearing ----------------------------------> done
+// @ TO DO : Style scroll-bar ------------------------------------> done
 // @ TO DO : Fix responsive landing page + intro
 // @ TO DO : Implemnt edited projects videos
 // @ TO DO : Implement ART section GIF
